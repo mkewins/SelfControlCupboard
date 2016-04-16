1f89472004bc5bc6476ddef38b34647c2f0188b3
Andrew Yang <ayang14@illinois.edu>
2016-04-16 17:59:40 +0200

    Start to restructure flask app

--- sccserver/sccserver/views.py
+++ sccserver/sccserver/views.py
@@ -2,12 +2,14 @@ from sccserver import app
 import fitbit, pprint
 from scripts.secrets import *
 from flask import redirect, request
+from firebase import firebase
 
 oauth = fitbit.FitbitOauth2Client(CLIENT_ID, CLIENT_SECRET)
 redirect_uri = 'http://localhost:1738/auth/callback'
 access_token = None
 refresh_token = None
 client = None
+ref = firebase.FirebaseApplication('https://snackattack.firebaseio.com', None)
 
 @app.route('/')
 def index():
@@ -19,7 +21,7 @@ def auth():
     return redirect(redirect_url)
 
 @app.route('/auth/callback')
-def authCallback():
+def auth_callback():
     code = request.args.get('code')
     state = request.args.get('state')
     results = oauth.fetch_access_token(code, redirect_uri)
@@ -31,10 +33,11 @@ def authCallback():
     client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, access_token=access_token)
     return 'Success'
 
-@app.route('/steps')
-def steps():
-    print access_token
+def fitbit_steps():
+    # pull steps data from fitbit and push to firebase
     if client is None:
-        return 'Need to authorize'
+        return redirect('/auth')
     stuff = client.activities()
-    return pprint.pprint(stuff)
+    steps = stuff['summary']['steps']
+    calories = stuff['summary']['calories']
+    firebase.put('/fitbit', {'steps': steps, 'calories': calories})
