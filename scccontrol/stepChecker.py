2512937119fdfef5cd349de90a4f016ed8be5259
Drew Warkentin <drewdulz@gmail.com>
2016-04-16 18:41:10 +0000

    cleaned up codebase

--- scccontrol/stepChecker.py
+++ scccontrol/stepChecker.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from firebase import firebase
 import RPi.GPIO as GPIO
 import time
@@ -10,11 +12,11 @@ p = GPIO.PWM(11,50)        #sets pin 11 to PWM and sends 50 signals per second
 #p.start(2.5)
 def lockDoor():
     	print 'LOCKING DOOR'
-	p.ChangeDutyCycle(7.5)
+	p.start(7.5)
 
 def unlockDoor():
     	print 'UNLOCKING DOOR'
-	p.ChangeDutyCycle(2.5)
+	p.start(2.5)
 
 
 firebase = firebase.FirebaseApplication('https://snackattack.firebaseio.com', None)
